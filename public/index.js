a09581306e21d9848f1b803359c9330d890eb226
Krzysztof <drkrzysztofzieba@gmail.com>
2024-06-18 13:37:02 +0200

    login

--- public/index.js
+++ public/index.js
@@ -1,1 +1,24 @@
-/*eslint-diable*/
+/* eslint-disable */
+const loginForm = document.querySelector('.form-login');
+
+loginForm.addEventListener('submit', async (e) => {
+  try {
+    e.preventDefault();
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const res = await axios({
+      method: 'post',
+      url: '/api/v1/login',
+      data: { email, password },
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Zalogowano pomyślnie');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+});
