8dd25cbc983703fe4971abbdf074ee1562f8a21c
Krzysztof <drkrzysztofzieba@gmail.com>
2024-06-18 14:28:34 +0200

    login one file

--- public/index.js
+++ public/index.js
@@ -1,25 +1,37 @@
 /* eslint-disable */
-import { showAlert } from './alerts';
 const loginForm = document.querySelector('.form-login');
 
-loginForm.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  try {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const res = await axios({
-      method: 'post',
-      url: '/api/v1/users/login',
-      data: { email, password },
-    });
+const hideAlert = () => {
+  const el = document.querySelector('.alert');
+  if (el) el.parentElement.removeChild(el);
+};
 
-    if (res.data.status === 'success') {
-      showAlert('success', 'Zalogowano pomyślnie');
-      window.setTimeout(() => {
-        location.assign('/');
-      }, 1500);
+const showAlert = (type, msg) => {
+  hideAlert();
+  const markup = `<div class="alert alert--${type}">${msg}</div>`;
+  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+  window.setTimeout(hideAlert, 5000);
+};
+
+if (loginForm)
+  loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    try {
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+      const res = await axios({
+        method: 'post',
+        url: '/api/v1/users/login',
+        data: { email, password },
+      });
+
+      if (res.data.status === 'success') {
+        showAlert('success', 'Zalogowano pomyślnie');
+        window.setTimeout(() => {
+          location.assign('/');
+        }, 1500);
+      }
+    } catch (err) {
+      showAlert('error', err.response.data.message);
     }
-  } catch (err) {
-    showAlert('error', err.response.data.message);
-  }
-});
+  });
